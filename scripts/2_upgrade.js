6943fa14ab41e835ce8e9dae5902af5a16f633b7
alex <67682756+alex-x-crypto@users.noreply.github.com>
2023-02-17 09:41:58 +0800

    feat: upgrade to V2

---
+++ scripts/2_upgrade.js
@@ -0,0 +1,13 @@
+// scripts/upgrade-box.js
+const { ethers, upgrades } = require("hardhat");
+
+async function main() {
+    const ScoreV2 = await ethers.getContractFactory("ScoreV2");
+    const instance = await upgrades.upgradeProxy('', ScoreV2);
+    console.log("Score V2 upgraded");
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
